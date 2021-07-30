d403c52d4c80d6cc28cf9b335d995cb8854987e0
Tyler Ma <tylermavan@gmail.com>
2021-07-30 02:53:40 -0500

    Switch 'bent' for 'axios'
    feat: Removed the bent module as there was not much references to follow when programming it. Axios is a very popular HTTP client for node and appears to have more references to use as examples.

--- app.js
+++ app.js
@@ -7,15 +7,11 @@ const url = 'https://zcccodingchallenge.zendesk.com/api/v2/tickets.json'
 const user = 'tylermavan@gmail.com' + '/token'
 const pwd = '3y6VNkmHyqUWIqMC8hdEfEXi82xsDJhwBzefZnYZ'
 
-const response = request.get(url, auth=(user, pwd));
-
 // Create our app by calling the express function
 const app = express();
 
 // Register a route handler for GET requests made to /hello
-app.post('/', (req, res) => {
-  res.
-});
+app.use(express.static(__dirname + '/public'));
 
 // Get port from environment or default to port 3000.
 const port = process.env.PORT || 3000;
