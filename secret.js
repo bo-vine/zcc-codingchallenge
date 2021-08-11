31462667c72c2e113ca15723921cc8f301fd2565
Tyler Ma <tylermavan@gmail.com>
2021-08-10 23:37:29 -0500

    Delete secret.js

--- secret.js
+++
@@ -1,8 +0,0 @@
-export function encryptAuthkey () {
-  let user = 'tylermavan@gmail.com' + '/token'
-  let pwd = '3y6VNkmHyqUWIqMC8hdEfEXi82xsDJhwBzefZnYZ'
-  let b64 = Buffer.from(user + ':' + pwd).toString('base64')
-  const basicAuth = 'Basic ' + b64
-
-  return basicAuth
-}
